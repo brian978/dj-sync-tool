def1faa74d412f9167ee3dff923137fbce80b4c5
brian978 <brian978@users.noreply.github.com>
2023-04-19 20:06:11 +0200

    Added marker decoders for AIFF files

---
+++ app/decoders/serato/aif/v1/AifDecoder.py
@@ -0,0 +1,14 @@
+# noinspection PyPep8Naming
+from mutagen import File as MutagenFile
+
+from app.decoders.serato.mp3.v1.Mp3Decoder import Mp3Decoder
+
+
+class AifDecoder(Mp3Decoder):
+    def _read_data_from_tags(self, filepath: str) -> bytes | None:
+        tags = MutagenFile(filepath)
+        if self.TAG_NAME in tags:
+            tag_data = tags[self.TAG_NAME]
+            return tag_data.data
+
+        return None
