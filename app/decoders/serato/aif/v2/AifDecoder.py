c9793ca96f261e6a1e3ba3ce89a5ff0ef6aea6e9
brian978 <brian978@users.noreply.github.com>
2023-04-19 20:12:36 +0200

    Fixed some bugs

--- app/decoders/serato/aif/v2/AifDecoder.py
+++ app/decoders/serato/aif/v2/AifDecoder.py
@@ -1,7 +1,8 @@
 # noinspection PyPep8Naming
-from mutagen import File as MutagenFile
+from mutagen import File as MutagenFile, id3
 
 from app.decoders.serato.mp3.v2.Mp3Decoder import Mp3Decoder
+from app.models.MusicFile import MusicFile
 
 
 class AifDecoder(Mp3Decoder):
@@ -12,3 +13,14 @@ class AifDecoder(Mp3Decoder):
             return tag_data.data
 
         return None
+
+    def _write_data_to_tags(self, music_file: MusicFile, payload: bytes):
+        mutagen_file = MutagenFile(music_file.location)
+        mutagen_file[self.TAG_NAME] = id3.GEOB(
+            encoding=0,
+            mime='application/octet-stream',
+            desc=self.MARKERS_NAME,
+            data=payload,
+        )
+
+        return mutagen_file
